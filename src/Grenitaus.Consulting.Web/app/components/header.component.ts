70380f30baa6073745c4b4b32779028b3dca3e47
michaeldeongreen <michael.green@architectinginnovation.com>
2017-02-15 18:56:00 -0600

    *Header component created and working

---
+++ src/Grenitaus.Consulting.Web/app/components/header.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: 'app/templates/header.component.html'
+})
+export class HeaderComponent { }
--- src/Grenitaus.Consulting.Web/app/modules/app.module.ts
+++ src/Grenitaus.Consulting.Web/app/modules/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from '../components/app.component';
+import { HeaderComponent } from '../components/header.component';
 import { FooterComponent } from '../components/footer.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, FooterComponent ],
+  imports:      [ BrowserModule, HttpModule ],
+  declarations: [ AppComponent, HeaderComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
