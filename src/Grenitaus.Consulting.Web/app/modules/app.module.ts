8f2c5b440d4516b858f52e91ca66c1932edb85d3
michaeldeongreen <michael.green@architectinginnovation.com>
2017-02-16 18:05:44 -0600

    *Removed code in app.module to handle refresh/404 and put rules in the web.config to handle the error.  Did this to get rid of the hashtag in the url

--- src/Grenitaus.Consulting.Web/app/modules/app.module.ts
+++ src/Grenitaus.Consulting.Web/app/modules/app.module.ts
@@ -9,14 +9,10 @@ import { AboutComponent } from '../components/about.component';
 import { HomeComponent } from '../components/home.component';
 import { AppRoutingModule } from './app-routing.module';
 
-//Added this import and the providers configuration to fix refresh 404 errors
-//http://stackoverflow.com/questions/35284988/angular-2-404-error-occur-when-i-refresh-through-browser
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   imports:      [ BrowserModule, HttpModule, AppRoutingModule ],
   declarations: [AppComponent, HeaderComponent, FooterComponent, AboutComponent, HomeComponent],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
